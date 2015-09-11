479866a67b728ef0d153a81e384013d4ca4a3ba3
simulo2000@gmail.com <H4nsW0rst>
2015-09-11 20:34:24 +0200

    added database configuration

---
+++ Server/TestServerScripts/Configuration/TestDatabaseConfiguration.php
@@ -0,0 +1,8 @@
+<?php
+
+$databaseName = 'solarschiff_dev';
+$databaseUserName = 'solarschiff_dev';
+$databaseUserPassword = null; 				// insert the correct password here manually
+$databaseHost = 'db4free.net';
+
+?>
