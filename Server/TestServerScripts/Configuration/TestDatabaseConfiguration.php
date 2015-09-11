c50dd1db2eb9ab8b072a9357385e402ec3a443c7
simulo2000@gmail.com <H4nsW0rst>
2015-09-11 20:37:15 +0200

    updated gitignore once again

--- Server/TestServerScripts/Configuration/TestDatabaseConfiguration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$databaseName = 'solarschiff_dev';
-$databaseUserName = 'solarschiff_dev';
-$databaseUserPassword = null; 				// insert the correct password here manually
-$databaseHost = 'db4free.net';
-
-?>
