8cdea2cfa6316aba74aafaa54f220df41f8903b8
simulo2000@gmail.com <H4nsW0rst>
2015-09-11 20:32:46 +0200

    added test-database setup scripts and connection test

---
+++ Server/Database/HighscoreTestSeed.sql
@@ -0,0 +1,6 @@
+USE `solarschiff_dev`;
+
+INSERT INTO `Highscore` (`Id`, `LastName`, `FirstName`, `DisplayName`, `Email`, `Score`, `ConfirmationStatus`) VALUES
+	(1, 'Mustermann', 'Hans', 'hansm85', 'hans@web.de', 1234, 0),
+	(2, 'Musterli', 'Peter', 'petermusterli83', 'm.peter@web.de', 2546, 0),
+	(3, 'Schwegler', 'Simon', 'perezite', 'geheim@geheim.de', 9999, 0);
